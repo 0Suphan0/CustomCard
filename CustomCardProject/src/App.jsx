c46b1212a8c21cb5041bca0a515c154acd868987
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2024-03-20 15:19:19 +0300

    Init

---
+++ CustomCardProject/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>Süphan</p>
+    
+    </>
+  )
+}
+
+export default App
