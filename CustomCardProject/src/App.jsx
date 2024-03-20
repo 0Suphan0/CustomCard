f6e2247b40945c09346d676126470d31ac76f776
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2024-03-20 16:24:47 +0300

    Css changed !

--- CustomCardProject/src/App.jsx
+++ CustomCardProject/src/App.jsx
@@ -1,52 +1,71 @@
+import "bulma/css/bulma.css";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Course from "./Course";
 
-import Angular from "./images/angular.jpg"
-import Bootstrap from "./images/bootstrap5.png"
-import Csharp from "./images/ccsharp.png"
-import Web from "./images/kompleweb.jpg"
-
-
+import Angular from "./images/angular.jpg";
+import Bootstrap from "./images/bootstrap5.png";
+import Csharp from "./images/ccsharp.png";
+import Web from "./images/kompleweb.jpg";
 
 function App() {
   return (
     <>
-      <Course
-        image={Angular}
-        title="Angular Kursu"
-        description="Lorem,
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Tüm Kurslarım</p>
+        </div>
+      </section>
+      <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <Course
+              image={Angular}
+              title="Angular Kursu"
+              description="Lorem,
          ipsum dolor sit amet consectetur adipisicing elit. 
          Eveniet rerum vitae reiciendis sint neque fugit, minima perferendis,
           non illo ratione quos recusandae tenetur asperiores nulla repellendus quia, repudiandae molestias aspernatur."
-      />
-      <Course
-       image={Bootstrap}
-        title="Bootstrap Kursu"
-        description="Lorem,
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Bootstrap}
+              title="Bootstrap Kursu"
+              description="Lorem,
          ipsum dolor sit amet consectetur adipisicing elit. 
          Eveniet rerum vitae reiciendis sint neque fugit, minima perferendis,
           non illo ratione quos recusandae tenetur asperiores nulla repellendus quia, repudiandae molestias aspernatur."
-      />
-      <Course
-        image={Csharp}
-        title="C# Kursu"
-        description="Lorem,
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Csharp}
+              title="C# Kursu"
+              description="Lorem,
          ipsum dolor sit amet consectetur adipisicing elit. 
          Eveniet rerum vitae reiciendis sint neque fugit, minima perferendis,
           non illo ratione quos recusandae tenetur asperiores nulla repellendus quia, repudiandae molestias aspernatur."
-      />
-      <Course
-        image={Web}
-        title="Web Kursu"
-        description="Lorem,
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Web}
+              title="Web Kursu"
+              description="Lorem,
          ipsum dolor sit amet consectetur adipisicing elit. 
          Eveniet rerum vitae reiciendis sint neque fugit, minima perferendis,
           non illo ratione quos recusandae tenetur asperiores nulla repellendus quia, repudiandae molestias aspernatur."
-      />
-    
+            />
+          </div>
+        </div>
+      </div>
+
+      </section>
+      
     </>
   );
 }
--- CustomCardProject/src/Course.jsx
+++ CustomCardProject/src/Course.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 
 const Course = ({image,title,description}) => {
   return (
-    <div>
-      <img src={image}/>  
-      <div>{title}</div>
-      <div>{description}</div>
-      
+<div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src={image} alt="Placeholder image"/>
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4">{title}</p>
+      </div>
     </div>
+
+    <div class="content">
+      {description}
+    </div>
+  </div>
+</div>
+
   )
 }
 
