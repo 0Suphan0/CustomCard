09043851f219c9c5e62afb4c129e0da2b36d38e1
Süphan Yakut <80632509+0Suphan0@users.noreply.github.com>
2024-03-20 15:43:18 +0300

    react props implemented.

--- CustomCardProject/src/App.jsx
+++ CustomCardProject/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Course from "./Course";
 
 function App() {
-
   return (
     <>
-    <p>Süphan</p>
+      <Course
+        title="Angular Kursu"
+        description="Lorem,
+         ipsum dolor sit amet consectetur adipisicing elit. 
+         Eveniet rerum vitae reiciendis sint neque fugit, minima perferendis,
+          non illo ratione quos recusandae tenetur asperiores nulla repellendus quia, repudiandae molestias aspernatur."
+      />
+      <Course
+        title="Bootstrap Kursu"
+        description="Lorem,
+         ipsum dolor sit amet consectetur adipisicing elit. 
+         Eveniet rerum vitae reiciendis sint neque fugit, minima perferendis,
+          non illo ratione quos recusandae tenetur asperiores nulla repellendus quia, repudiandae molestias aspernatur."
+      />
+      <Course
+        title=".Net Kursu"
+        description="Lorem,
+         ipsum dolor sit amet consectetur adipisicing elit. 
+         Eveniet rerum vitae reiciendis sint neque fugit, minima perferendis,
+          non illo ratione quos recusandae tenetur asperiores nulla repellendus quia, repudiandae molestias aspernatur."
+      />
+      <Course
+        title="Java Kursu"
+        description="Lorem,
+         ipsum dolor sit amet consectetur adipisicing elit. 
+         Eveniet rerum vitae reiciendis sint neque fugit, minima perferendis,
+          non illo ratione quos recusandae tenetur asperiores nulla repellendus quia, repudiandae molestias aspernatur."
+      />
     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ CustomCardProject/src/Course.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Course = ({title,description}) => {
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{description}</div>
+    </div>
+  )
+}
+
+export default Course
